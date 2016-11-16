7933850f0accc901ab9d67bed7667702639ec553
mikesfilip <filip.mikes@email.cz>
2016-11-16 21:06:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int main()
 {
     int arr[3] = {12,4,1};
     // print to console
+    // new update server
     printf("Here is the array %d, %d, %d, %d\n", arr[0], arr[1], arr[2], arr[3]);
     // return from main
+    // second update on server
     return 0;
 }
