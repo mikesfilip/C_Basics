f8744cb47fb02febd18309d1ef7ea03b3ddd9b7b
Filip Mikes <filip@mikes.com>
2016-11-16 19:59:52 +0100

    Prvni commit pro C_Basics

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[3] = {12,4,1};
+
+    printf("Here is the array %d, %d, %d, %d\n", arr[0], arr[1], arr[2], arr[3]);
+
+    return 0;
+}
