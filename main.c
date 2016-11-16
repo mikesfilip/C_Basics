7cda9ddfd9a0c0465cafa2776382541d5396f96a
Filip Mikes <filip.mikes@email.cz>
2016-11-16 21:09:29 +0100

    Main.c locally updated

--- main.c
+++ main.c
@@ -7,5 +7,6 @@ int main()
     // print to console
     printf("Here is the array %d, %d, %d, %d\n", arr[0], arr[1], arr[2], arr[3]);
     // return from main
+    // third commnet localy
     return 0;
 }
